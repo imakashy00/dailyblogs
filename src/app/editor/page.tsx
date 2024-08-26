af04917d87e27a7ae8aa11bc2970412463a0edd2
Akash Yadav <imakashy00@gmail.com>
2024-08-27 04:07:40 +0530

    removed editor

--- src/app/editor/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Editor from '@/components/Editor'
-export default function page() {
-  return (
-    <div className='py-40 w-4/5 pl-80 flex justify-center'>
-      <Editor/>
-    </div>
-  )
-}
