1b0d1ed3b44771641cdb3c1378d7f107ec1bab65
Akash Yadav <imakashy00@gmail.com>
2024-08-28 15:50:26 +0530

    corrected signup redirect

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -10,7 +10,9 @@ import { auth } from "@/auth";
 export default async function SignUp() {
   const session = await auth();
   const user = session?.user;
-  redirect("/dashboard");
+  if (user) {
+    redirect("/dashboard");
+  }
   return (
     <div className="mt-10 max-w-md w-full mx-auto rounded md:rounded-2xl p-4 md:p-8 shadow-input bg-white border border-[#121212] dark:bg-black">
       <h2 className="font-bold text-2xl text-neutral-800">
