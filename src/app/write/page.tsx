facfa79995f2cdf4dcd39dce19545fbd292280b9
Akash Yadav <imakashy00@gmail.com>
2024-08-28 17:37:50 +0530

    transfered editor to page

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,48 +1,9 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import { BlockNoteSchema, defaultBlockSpecs } from "@blocknote/core";
-import "@blocknote/core/fonts/inter.css";
-import { useCreateBlockNote } from "@blocknote/react";
-import { BlockNoteView } from "@blocknote/mantine";
-import "@blocknote/mantine/style.css";
+const Editor = dynamic(() => import("../../components/Editor"), {
+  ssr: false,
+});
 
-async function uploadFile(file: File) {
-  const body = new FormData();
-  body.append("file", file);
-
-  //   const ret = await fetch("https://tmpfiles.org/api/v1/upload", {
-  //     method: "POST",
-  //     body: body,
-  //   });
-  //   return (await ret.json()).data.url.replace(
-  //     "tmpfiles.org/",
-  //     "tmpfiles.org/dl/"
-  //   );
-}
-export default function Editor() {
-  const schema = BlockNoteSchema.create({
-    blockSpecs: {
-      ...defaultBlockSpecs,
-      audio: undefined as any,
-      file: undefined as any,
-      video: undefined as any,
-    },
-  });
-
-  // Creates a new editor instance.
-  const editor = useCreateBlockNote({
-    schema,
-    // uploadFile,
-  });
-
-  // Renders the editor instance using a React component.
-  return (
-    <BlockNoteView
-      className="w-full bn-editor -m-20"
-      editor={editor}
-      sideMenu={true}
-      editable={true}
-      theme={"light"}
-    />
-  );
+export default function WritePage() {
+  return <Editor />;
 }
