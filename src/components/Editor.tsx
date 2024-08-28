3730aa71b7453f4d40a2a41e37cc22e808887b30
Akash Yadav <imakashy00@gmail.com>
2024-08-28 22:14:51 +0530

    redirect to editor instead of dashboard

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -37,6 +37,31 @@ export default function Editor() {
   const editor = useCreateBlockNote({
     schema,
     uploadFile,
+    initialContent: [
+      {
+        type: "heading",
+        content: "Welcome 🙏",
+      },
+      { type: "paragraph", content: "" },
+      {
+        type: "paragraph",
+        content: "type '/' to get suggestions",
+      },
+      {
+        type: "bulletListItem",
+        content:
+          "Insert heading, paragraph,bulletpoint image,emoji and text color on selecting text.. ",
+      },
+      {
+        type: "bulletListItem",
+        content: "Please signUp for launch discount",
+      },
+      {
+        type: "bulletListItem",
+        content:
+          "Please share your feedback and suggestions 📧  yakashadav26@gmail.com",
+      },
+    ],
   });
 
   // Renders the editor instance using a React component.
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -231,7 +231,7 @@ const Landing = () => {
                 <Zap className="mr-2 text-green-400 fill-green-400 border-1 border-green-400 border rounded-full size-8" />
                 Pro
               </h2>
-              <h3 className="py-5 text-2xl text-gray-700">$5 / month</h3>
+              <h3 className="py-5 text-2xl text-gray-700">$10 / month</h3>
               <h4 className="text-lg text-gray-500">What&apos;s Included:</h4>
               <ul className="mt-5">
                 <li className="my-5 flex text-gray-800">
@@ -270,7 +270,7 @@ const Landing = () => {
                 <Zap className="mr-2 text-yellow-400 fill-yellow-400 border-1 border-yellow-400 border rounded-full size-8" />{" "}
                 Ultimate
               </h2>
-              <h3 className="py-5 text-2xl text-gray-700">$10 / month</h3>
+              <h3 className="py-5 text-2xl text-gray-700">$20 / month</h3>
               <h4 className="text-lg text-gray-500">What&apos;s Included:</h4>
               <ul className="mt-5">
                 <li className="my-5 flex text-gray-800">
