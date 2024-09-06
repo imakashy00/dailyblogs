2af8cb928ef5af8ee75e234bb74d9f7fbfd8d5dc
Akash Yadav <imakashy00@gmail.com>
2024-09-06 23:17:59 +0530

    polished regidtration and added writing page

--- src/app/api/dashboard/route.ts
+++ src/app/api/dashboard/route.ts
@@ -1,2 +1,7 @@
 // fetch details for dashboard such as current streak, longest streak, lvl,  
 
+import { NextRequest } from "next/server";
+
+export default async function GET(request: NextRequest) {
+    return new Response("Hello, Next.js!");
+}
