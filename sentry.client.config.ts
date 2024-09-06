2e2fa4fdabbec8c0ff846848dc83cd1706457014
Akash Yadav <imakashy00@gmail.com>
2024-09-07 02:25:36 +0530

    added signout button and logged in user name on navbar

---
+++ sentry.client.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://d562ebfbfceb0465cc09fadf46fc7d56@o4507907889233920.ingest.us.sentry.io/4507907891724288",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
